7bc47d6c55ef40b24946d9bddbf2699664e5d3a1
kevin Martínez <kevinccbsg@gmail.com>
2019-12-09 17:02:54 +0100

    Add test to check if processor prints when it is a palindrome

--- test/ohce.test.js
+++ test/ohce.test.js
@@ -1,13 +1,27 @@
 const { procesor } = require('..');
 
-jest.spyOn(global.console, 'log');
+const spy = jest.spyOn(global.console, 'log');
 
 describe('Ohce', () => {
+  beforeEach(() => {
+    spy.mockClear();
+  });
+
   it('should print reverse input in console', () => {
     const input = 'Message';
     const result = 'egasseM';
     procesor(input);
     expect(console.log).toBeCalled();
     expect(console.log).toHaveBeenCalledWith(result);
+  });
+
+  it('should print the input and this sentence "¡Bonita palabra!" when input is a palindrome', () => {
+    const input = 'oto';
+    const firstPrint = input;
+    const secondPrint = '¡Bonita palabra!';
+    procesor(input);
+    expect(console.log).toHaveBeenCalledTimes(2);
+    expect(console.log).toHaveBeenNthCalledWith(1, firstPrint);
+    expect(console.log).toHaveBeenNthCalledWith(2, secondPrint);
   });
 });
