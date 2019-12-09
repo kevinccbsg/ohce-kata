667ab41afc90addca39962685ed47fede1dc490c
ismaelocaramelo <ismaelbacha@hotmail.com>
2019-12-09 19:04:38 +0100

    Use composition :)

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const { reverse, isPalindrome, greets } = require('./lib');
+const pipe = require('./utils/pipe');
 
 const init = args => {
   const name = args[2];
   const time = new Date().getTime();
+
+  const _isPalindrome = input => {
+    if (isPalindrome(input)) console.log('¡Bonita palabra!');
+    return input;
+  };
+
+  const _reverse = input => {
+    const reverseInput = reverse(input);
+    console.log(reverseInput);
+    return input;
+  };
+
+  const _stop = (input, cb) => {
+    if (input === 'Stop!') {
+      console.log(`Adios ${name}`);
+      return process.exit(0);
+    }
+    return cb(input);
+  };
 
   const greet = () => {
     const greeting = greets({ time, name });
     return console.log(greeting);
   };
 
-  const procesor = input => {
-    const reverseInput = reverse(input);
-    console.log(reverseInput);
-    if (isPalindrome(input)) console.log('¡Bonita palabra!');
-  };
+  const processor = input => _stop(input, pipe(_reverse, _isPalindrome));
 
-  return { procesor, greet };
+  return { processor, greet };
 };
 
 module.exports = init;
