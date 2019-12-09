3c6dd71d28cd61896d1ef7abde1fd363936c3390
kevin Martínez <kevinccbsg@gmail.com>
2019-12-09 16:48:25 +0100

    Add second isPalindrome method condition

--- test/unit/palindrome.test.js
+++ test/unit/palindrome.test.js
@@ -1,9 +1,16 @@
 const { isPalindrome } = require('../../lib');
 
 describe('isPalindrome method tests', () => {
-  it('should return false when it is not a palindrome', () => {
+  it('should return false when input is not a palindrome', () => {
     const input = 'NoPalindrome';
     const expected = false;
+    const result = isPalindrome(input);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return true when input is a palindrome', () => {
+    const input = 'oto';
+    const expected = true;
     const result = isPalindrome(input);
     expect(result).toEqual(expected);
   });
