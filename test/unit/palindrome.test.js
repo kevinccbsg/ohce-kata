fb73f5c33bbc798f2044e4c4850d72fe5d2dd49a
kevin Martínez <kevinccbsg@gmail.com>
2019-12-09 16:41:55 +0100

    Test isPalindrome method

---
+++ test/unit/palindrome.test.js
@@ -0,0 +1,9 @@
+
+describe('isPalindrome method tests', () => {
+  it('should return false when it is not a palindrome', () => {
+    const input = 'NoPalindrome';
+    const expected = false;
+    const result = isPalindrome(input);
+    expect(result).toEqual(expected);
+  });
+});
