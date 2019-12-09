7d12b2c2349af3ac4d9b198eecd823d011564aad
kevin Martínez <kevinccbsg@gmail.com>
2019-12-09 18:19:50 +0100

    Refactor package interface to add a name context for process function

--- test/ohce.test.js
+++ test/ohce.test.js
@@ -1,35 +1,38 @@
-const { procesor, initGreets } = require('..');
+const init = require('..');
 
 const spy = jest.spyOn(global.console, 'log');
+
 
 describe('Ohce', () => {
   beforeEach(() => {
     spy.mockClear();
   });
 
-
   it('should print greeting in console', () => {
-    const args = [undefined, null, 'Ismael'];
-    initGreets(args);
+    const { greet } = init([null, null, 'Ismael']);
+    greet();
     expect(console.log).toBeCalled();
     expect(console.log).toHaveBeenCalledWith(expect.stringMatching(/Ismael/));
   });
 
-  it('should print reverse input in console', () => {
-    const input = 'Message';
-    const result = 'egasseM';
-    procesor(input);
-    expect(console.log).toBeCalled();
-    expect(console.log).toHaveBeenCalledWith(result);
-  });
+  describe('procesor', () => {
+    const { procesor } = init([null, null, 'Ismael']);
+    it('should print reverse input in console', () => {
+      const input = 'Message';
+      const result = 'egasseM';
+      procesor(input);
+      expect(console.log).toBeCalled();
+      expect(console.log).toHaveBeenCalledWith(result);
+    });
 
-  it('should print the input and this sentence "¡Bonita palabra!" when input is a palindrome', () => {
-    const input = 'oto';
-    const firstPrint = input;
-    const secondPrint = '¡Bonita palabra!';
-    procesor(input);
-    expect(console.log).toHaveBeenCalledTimes(2);
-    expect(console.log).toHaveBeenNthCalledWith(1, firstPrint);
-    expect(console.log).toHaveBeenNthCalledWith(2, secondPrint);
+    it('should print the input and this sentence "¡Bonita palabra!" when input is a palindrome', () => {
+      const input = 'oto';
+      const firstPrint = input;
+      const secondPrint = '¡Bonita palabra!';
+      procesor(input);
+      expect(console.log).toHaveBeenCalledTimes(2);
+      expect(console.log).toHaveBeenNthCalledWith(1, firstPrint);
+      expect(console.log).toHaveBeenNthCalledWith(2, secondPrint);
+    });
   });
 });
